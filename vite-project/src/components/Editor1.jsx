fa533f6606f028a0a8a89bedc15ed149141f94d7
Affan <affanatweb@gmail.com>
2024-03-01 02:08:41 +0500

    FEAT: Converted to Class Based Component

--- vite-project/src/components/Editor1.jsx
+++ vite-project/src/components/Editor1.jsx
@@ -1,30 +1,33 @@
 import React, { Component } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import Editor  from 'ckeditor5-custom-build/build/ckeditor';
-const Editor1=()=> {
+
+class Editor1 extends Component {
     
-        return (
-            <div className="App">
-                <h2>Using CKEditor 5 build in React</h2>
-                <CKEditor
-                    editor={ Editor }
-                    data="<p>Hello from CKEditor 5!</p>"
-                    onReady={ editor => {
-                        // Allows you to store the editor instance and use it later.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-            </div>
-        );
+        return(){
+           
+                <div className="App">
+                    <h2>Using CKEditor 5 build in React</h2>
+                    <CKEditor
+                        editor={ Editor }
+                        data="<p>Hello from CKEditor 5!</p>"
+                        onReady={ editor => {
+                            // Allows you to store the editor instance and use it later.
+                            console.log( 'Editor is ready to use!', editor );
+                        } }
+                        onChange={ ( event, editor ) => {
+                            const data = editor.getData();
+                            console.log( { event, editor, data } );
+                        } }
+                        onBlur={ ( event, editor ) => {
+                            console.log( 'Blur.', editor );
+                        } }
+                        onFocus={ ( event, editor ) => {
+                            console.log( 'Focus.', editor );
+                        } }
+                    />
+                </div>
+
+        }
     }
 export default Editor1;
